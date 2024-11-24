54eb2669f23958ad50d879ce4ff6c0eaed1acd2a
Ehtisham Zahid <ehtishamzahid039@gmail.com>
2024-11-24 21:51:05 +0500

    Mongo Setup and user model created

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,15 @@
-import express from 'express'; //web framework for building apis giving routes, middleware
+import express from "express"; //web framework for building apis giving routes, middleware
 import authRoutes from "./routes/auth.route.js"
-
+import dotenv from "dotenv"
+import { connectDB } from "./lib/db.js";
 
 const app = express();
 
+dotenv.config();
+const PORT = process.env.PORT;
 
-app.use('/api/auth',authRoutes)
-app.listen(5001, (req, res) => {
-    console.log(`Server is listening on port 5001 `)
+app.use("/api/auth", authRoutes)
+app.listen(PORT, (req, res) => {
+    console.log(`Server is listening on port ${PORT} `)
+    connectDB();
 })
