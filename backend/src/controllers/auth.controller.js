1e02ab3f37080b0c945dc9f8bf3f635e5d516c37
Ehtisham Zahid <ehtishamzahid039@gmail.com>
2024-12-02 14:30:45 +0500

    Changes to be committed: 	modified:   backend/src/controllers/auth.controller.js

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -9,7 +9,7 @@ export const signup = async (req, res) => {
         if (!fullName || !email || !password) {
             return res.status(400).json({ message: "All feilds are required." })
         }
-        if (password < 6) {
+        if (password.length < 6) {
             return res.status(400).json({ message: "Password must be at least 6 characters." })
         }
 
@@ -40,12 +40,37 @@ export const signup = async (req, res) => {
             res.status(400).json({ message: "Invalid user data" })
         }
     } catch (error) {
-        console.log("error in signup controller")
+        console.log("error in signup controller", error.message);
+        res.status(500).json({ message: "Internal Server Error" });
     }
 }
 
-export const login = (req, res) => {
-    res.send("login route")
+export const login = async (req, res) => {
+    const { email, password } = req.body;
+    try {
+        const user = await User.findOne({ email });
+        console.log(typeof (user))
+        if (!user) {
+            res.status(400).json({ message: "Invalid Credentaials1!" })
+        }
+
+        const isPasswordCorrect = await bcrypt.compare(password, user.password);
+
+        if (!isPasswordCorrect) {
+            res.status(400).json({ message: "Invalid Credentaials2!" })
+        }
+
+        generateToken(user._id, res);
+        res.status(200).json({
+            _id: user._id,
+            fullName: user.fullName,
+            email: user.email,
+            profilePic: user.profilePic
+        })
+    } catch (error) {
+        console.log("Error in login controller", error.message);
+        res.status(500).json({ message: "Internal Server Error!" })
+    }
 }
 
 export const logout = (req, res) => {
