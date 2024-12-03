1f98272681a520cd28617fb53054481c3220ab1d
Ehtisham Zahid <ehtishamzahid039@gmail.com>
2024-12-03 21:50:52 +0500

    Changes to be committed: 	modified:   backend/src/index.js 	new file:   backend/src/models/message.model.js 	new file:   backend/src/routes/message.route.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,6 @@
 import express from "express"; //web framework for building apis giving routes, middleware
 import authRoutes from "./routes/auth.route.js"
+import messageRoutes from "./routes/message.route.js"
 import dotenv from "dotenv"
 import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser"
@@ -15,6 +16,7 @@ app.use(cookieParser())
 
 
 app.use("/api/auth", authRoutes)
+app.use("/api/message", messageRoutes)
 
 
 app.listen(PORT, (req, res) => {
