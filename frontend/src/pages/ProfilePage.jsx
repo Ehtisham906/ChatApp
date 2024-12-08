50cb54ba914ba406299e8c6c764988dcdb850ef3
Ehtisham Zahid <ehtishamzahid039@gmail.com>
2024-12-08 20:54:19 +0500

    Changes to be committed: 	modified:   backend/src/controllers/auth.controller.js 	modified:   frontend/src/pages/ProfilePage.jsx 	modified:   frontend/src/store/useAuthStore.js

--- frontend/src/pages/ProfilePage.jsx
+++ frontend/src/pages/ProfilePage.jsx
@@ -1,6 +1,17 @@
+import { useAuthStore } from "../store/useAuthStore"
+
 const ProfilePage = () => {
+  const { authUser, isUpdatingProfile, updateProfile } = useAuthStore();
+  const handleImageUpload = async (e) => { };
   return (
-    <div>ProfilePage</div>
+    <div className="h-screen pt-20">
+      <div className="max-w-2xl mx-auto p-4 py-8">
+        <div className="bg-base-300 rounded-xl p-6 space-y-8">
+          <h1 className="text-2xl font-semibold">Profile</h1>
+          <p className="mt-2">Your profile In</p>
+        </div>
+      </div>
+    </div>
   )
 }
 
