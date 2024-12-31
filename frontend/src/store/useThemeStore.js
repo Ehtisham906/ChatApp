4e0c971d5fe1e3fff86bf65cd10163f4562a31e2
Ehtisham Zahid <ehtishamzahid039@gmail.com>
2024-12-31 17:55:01 +0500

    dlt

--- frontend/src/store/useThemeStore.js
+++
@@ -1,9 +0,0 @@
-// import { create } from "zustand";
-
-// export const useThemeStore = create((set) => ({
-//     theme: localStorage.getItem("chatTheme") || "coffee",
-//     setTheme: (theme) => {
-//         localStorage.setItem("chatTheme", theme);
-//         set({ theme }); 
-//     }
-// }))
