426f8d2f4e976b80157d11d60f0c3804826f69b9
Ehtisham Zahid <ehtishamzahid039@gmail.com>
2025-01-01 20:24:34 +0500

    ui changes

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,177 +1,55 @@
-// import { LogOut, LucidePodcast, Plane, User } from "lucide-react";
-// import { Link } from "react-router-dom";
-// import { useAuthStore } from "../store/useAuthStore"
+import { LogOut, LucidePodcast, Plane, User } from "lucide-react";
+import { Link } from "react-router-dom";
+import { useAuthStore } from "../store/useAuthStore"
 
-// const Navbar = () => {
-//   const { logout, authUser } = useAuthStore();
+const Navbar = () => {
+  const { logout, authUser } = useAuthStore();
 
-//   return (
-//     <header className=" bg-base-100 border-b border-base-300 sticky w-full top-0 z-40 backdrop-blur-lg bg-base-100/80">
-//       <div className="container max-auto px-4 h-16">
-//         <div className="grid grid-cols-3 items-center justify-between h-full">
-//           <div className="flex  items-center gap-8">
-//             <Link to="/" className="flex items-center gap-2.5 hover:opacity-80 transition-all">
-//               <div className="size-9 rounded-lg bg-pink/10 flex items-center justify-center">
-//                 <LucidePodcast className="w-5 h-5  " />
-//               </div>
-//               <h1 className="text-lg font-bold">Talkify</h1>
-//             </Link>
-//           </div>
-
-//           <div>
-//             <ul className="flex justify-between">
-//               <li className="cursor-pointer">Home</li>
-//               <li className="cursor-pointer">Features</li>
-//               <li className="cursor-pointer">About Us</li>
-//             </ul>
-//           </div>
-
-// <div className="flex justify-end gap-2"> 
-
-//   <Link
-//    to={"/login"}
-//   className={`btn btn-sm gap-2 transition-colors ${authUser ? "hidden" : ""}`}
-//   >
-
-//     <Plane className="size-4" />
-//     <span className="hidden sm:inline">Get Started</span>
-//   </Link>
-
-//   {authUser && (
-//     <>
-//       <Link to={"/profile"} className="btn btn-sm gap-2">
-//         <User className="size-5" />
-//         <span className="hidden sm:inline">Profile</span>
-//       </Link>
-
-//       <button className="flex gap-2 items-center" onClick={logout}>
-//         <LogOut className="size-5" />
-//         <span className="hidden sm:inline">Logout</span>
-//       </button>
-//     </>
-//   )}
-// </div>
-//         </div>
-//       </div>
-
-//     </header>
-//   )
-// }
-
-// export default Navbar
-
-
-
-import { LogOut, LucidePodcast, Plane, User } from 'lucide-react';
-import './Navbar.css';
-// import logo from '../assets/images/logo.webp'
-import { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
-import { useAuthStore } from '../store/useAuthStore';
-
-function Navbar() {
-
-  const { logout, authUser} = useAuthStore();
-  const [navItems, setNavItems] = useState(true);
-  const [isScrolled, setIsScrolled] = useState(false);
-  function toogleNavbar() {
-    setNavItems(!navItems)
-  }
-  useEffect(() => {
-    const handleScroll = () => {
-      if (window.scrollY > 100.99999999999999) {
-        setIsScrolled(true);
-      } else {
-        setIsScrolled(false);
-      }
-    };
-
-    window.addEventListener('scroll', handleScroll);
-
-    return () => {
-      window.removeEventListener('scroll', handleScroll);
-    };
-  }, []);
   return (
-    <nav
-      className={`navbar z-20   items-center justify-around ${isScrolled ? '' : 'px-3 py-2'}`}>
-      <div className="logo">
+    <header className=" bg-base-100 border-b border-base-300 sticky w-full top-0 z-40 backdrop-blur-lg bg-base-100/80">
+      <div className="container mx-auto px-4 h-16">
+        <div className="grid grid-cols-2 items-center justify-around h-full">
+          <div className="flex  items-center gap-8">
+            <Link to="/" className="flex items-center gap-2.5 hover:opacity-80 transition-all">
+              <div className="size-9 rounded-lg bg-pink/10 flex items-center justify-center">
+                <LucidePodcast className="w-5 h-5  " />
+              </div>
+              <h1 className="text-lg font-bold">Talkify</h1>
+            </Link>
+          </div>
+          <div className="flex justify-end gap-2">
 
-        <div className="flex  gap-8">
-          <Link to="/" className="flex items-center gap-2.5 hover:opacity-80 transition-all">
-            <div className="size-9 rounded-lg flex items-center justify-center">
-              <LucidePodcast className="w-5 h-5  " />
-            </div>
-            <h1 className="text-lg font-bold">Talkify</h1>
-          </Link>
-        </div>
-        <button className="navbar-toggler" onClick={toogleNavbar} aria-label={navItems ? "Close menu" : "Open menu"}>
-          <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 24 24" transform="matrix(-1, 0, 0, -1, 0, 0)">
-            {navItems ? (
+            <Link
+              to={"/login"}
+              className={`flex items-center bg-[#250066] text-white px-3 py-2 rounded-lg gap-2 transition-colors ${authUser ? "hidden" : ""}`}
+            >
+
+              <Plane className="size-4" />
+              <span className="hidden sm:inline">Get Started</span>
+            </Link>
+
+            {authUser && (
               <>
-                <g id="SVGRepo_bgCarrier" strokeWidth="0"></g><g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g><g id="SVGRepo_iconCarrier"> <g id="Menu / Menu_Alt_02"> <path id="Vector" d="M11 17H19M5 12H19M11 7H19" stroke="#262626" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"></path> </g> </g>
-              </>
-            ) : (
-              <>
-                <g id="SVGRepo_bgCarrier" strokeWidth="0"></g><g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M19 5L5 19M5 5L9.5 9.5M12 12L19 19" stroke="#262626" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"></path> </g>
+                <Link to={"/profile"} className="btn btn-sm gap-2">
+                  <User className="size-5" />
+                  <span className="hidden sm:inline">Profile</span>
+                </Link>
+
+                <button className="flex gap-2 items-center" onClick={logout}>
+                  <LogOut className="size-5" />
+                  <span className="hidden sm:inline">Logout</span>
+                </button>
               </>
             )}
-          </svg>
-        </button>
-      </div>
-      <div className={`nav-items ${navItems ? "close" : "open"} ${isScrolled ? 'top-[84px] lg:top-0' : 'top-[110px]  lg:top-0'} cursor-pointer `} >
-        <Link to="/" className="nav-links">
-          <div className='childLink relative '>
-            <div className='flex gap-1 bottom-1'>
-              <h1 className=' '>
-                Home
-              </h1>
-
-            </div>
           </div>
-        </Link>
-
-        <Link to="/about-us" className="nav-links">
-          <div className="childLink  realtive">
-            <h1 className=''>
-              About Us
-            </h1>
-          </div>
-        </Link>
-
-        <div className="flex justify-end gap-2">
-
-          <Link
-            to={"/login"}
-            className={`btn btn-sm gap-2 transition-colors ${authUser ? "hidden" : ""}`}
-          >
-
-            <Plane className="size-4" />
-            <span className="hidden sm:inline">Get Started</span>
-          </Link>
-
-          {authUser && (
-            <>
-              <Link to={"/profile"} className="btn btn-sm gap-2">
-                <User className="size-5" />
-                <span className="hidden sm:inline">Profile</span>
-              </Link>
-
-              <button className="flex gap-2 items-center" onClick={logout}>
-                <LogOut className="size-5" />
-                <span className="hidden sm:inline">Logout</span>
-              </button>
-            </>
-          )}
         </div>
-
-
-
       </div>
 
-      <div className={`w-[85%] absolute border-b  ${isScrolled ? 'bottom-0' : 'bottom-1'}`}>
-      </div>
-    </nav>
-  );
+    </header>
+  )
 }
-export default Navbar;
+
+export default Navbar
+
+
+
--- frontend/src/pages/LandingPage.jsx
+++ frontend/src/pages/LandingPage.jsx
@@ -1,9 +1,9 @@
 import { useRef } from 'react'
 import TextScrambler from '../components/textscrambler/TextScrambler'
 import useOnScreen from '../components/animation/useOnScreen'
+import { Clock, LockKeyhole, LucideRocket, MoveRight, Smile } from 'lucide-react';
 
-// images
-import landingHeroImage from '../assets/images/landingPage.png'
+// images 
 import lines from '../assets/images/line.png'
 import group from '../assets/images/Group.png'
 import group1 from '../assets/images/Group-1.png'
@@ -24,26 +24,26 @@ const LandingPage = () => {
             <section className='overflow-hidden '
                 ref={sectionRef0}
             >
-                <div className=' md:flex justify-around items-center'>
-                    <div className='ml-8 md:ml-0 text-[#262626] md:w-[45%] relative'>
+                <div className='  sm:flex justify-around items-center'>
+                    <div className='flex justify-center sm:block sm:ml-8 md:ml-4 text-[#262626] sm:w-[35%] relative'>
                         <h1 className='text-4xl lg:text-6xl '>
-                            <TextScrambler targetWord={"Get the best "} animationSpeed={40} />
+                            <TextScrambler targetWord={"Get the best "} animationSpeed={80} />
                             <span className='font-bold text-[#250066]'>
-                                <TextScrambler targetWord={"experience "} animationSpeed={40} />
+                                <TextScrambler targetWord={"experience "} animationSpeed={80} />
                             </span>
                             <span>
-                                <TextScrambler targetWord={"when sending  "} animationSpeed={40} />
+                                <TextScrambler targetWord={"when sending  "} animationSpeed={80} />
                             </span>
                             <span>
-                                <TextScrambler targetWord={"text"} animationSpeed={40} />
+                                <TextScrambler targetWord={"text"} animationSpeed={80} />
                             </span>
 
                             <span className='font-bold text-[#250066]'>
-                                <TextScrambler targetWord="messages." animationSpeed={40} />
+                                <TextScrambler targetWord="messages." animationSpeed={80} />
                             </span>
                         </h1>
 
-                        <div className='absolute -top-10'>
+                        <div className='hidden sm:visible absolute -top-10'>
                             <img
                                 src={lines}
                                 alt=""
@@ -51,21 +51,21 @@ const LandingPage = () => {
                             />
 
                         </div>
-                        <div className='absolute top-10 right-0'>
+                        <div className='hidden sm:visible absolute top-10 right-0'>
                             <img
                                 src={lines}
                                 alt=""
                                 width={100}
                             />
                         </div>
-                        <div className='absolute top-16 right-0'>
+                        <div className='hidden sm:visible absolute top-16 right-0'>
                             <img
                                 src={lines}
                                 alt=""
                                 width={100}
                             />
                         </div>
-                        <div className='absolute -bottom-10 left-20'>
+                        <div className='hidden sm:visible absolute -bottom-10 left-20'>
                             <img
                                 src={lines}
                                 alt=""
@@ -73,8 +73,8 @@ const LandingPage = () => {
                             />
                         </div>
                     </div>
-                   
-                    <div className=' mx-auto w-[90%] md:w-[45%] grid mt-4 md:mt-0  grid-cols-3 items-center    '>
+
+                    <div className='mx-auto sm:mx-0 w-[60%] sm:w-[35%] grid mt-4 md:mt-0  grid-cols-3 items-center    '>
 
                         <div className={`transition-transform duration-[3s] ease-in-out ${isVisible0 ? 'translate-x-0 opacity-100' : '-translate-x-full opacity-0'}`}>
                             <img src={group6} alt="" width={200} />
@@ -101,12 +101,62 @@ const LandingPage = () => {
             </section>
 
 
-            <section className='mt-[969px] overflow-hidden'
-            >
-                <h1 className='text-[90px] text-red-900'>
-                    Hello
-                </h1>
-                s
+            <section className='overflow-hidden mt-24 sm:flex justify-around'>
+                <div className='sm:w-[35%] grid grid-cols-2 gap-7 '>
+                    <div className='bg-[#FFF261] py-6 px-2 rounded-l-3xl rounded-br-3xl  flex flex-col items-center'>
+                        <div className='w-16 h-16 flex justify-center items-center bg-[#262626] rounded-full'>
+                            <LucideRocket className='w-8 h-8 text-white' />
+                        </div>
+
+                        <div className='text-center text-[#262626]'>
+                            <h1 className='text-2xl '>Fast</h1>
+                            <p className='text-sm'>Fast servers, connect easily and instantly.</p>
+                        </div>
+                    </div>
+                    <div className='bg-[#FFF261] py-6 px-2 rounded-l-3xl rounded-tr-3xl  flex flex-col items-center'>
+                        <div className='w-16 h-16 flex justify-center items-center bg-[#262626] rounded-full'>
+                            <Smile className='w-8 h-8 text-white' />
+                        </div>
+                        <div className='text-center text-[#262626]'>
+                            <h1 className='text-2xl '>Easy to use</h1>
+                            <p className='text-sm'>Simple and easy to use and instantly.</p>
+                        </div>
+                    </div>
+                    <div className='bg-[#FFF261] py-6 px-2 rounded-l-3xl rounded-tr-3xl  flex flex-col items-center'>
+                        <div className='w-16 h-16 flex justify-center items-center bg-[#262626] rounded-full'>
+                            <Clock  className='w-8 h-8 text-white' />
+                        </div>
+                        <div className='text-center text-[#262626]'>
+                            <h1 className='text-2xl '>Real Time</h1>
+                            <p className='text-sm'>Connect with your loved ones more faster</p>
+                        </div>
+                    </div>
+                    <div className='bg-[#FFF261] py-6 px-2 rounded-l-3xl rounded-tr-3xl  flex flex-col items-center'>
+                        <div className='w-16 h-16 flex justify-center items-center bg-[#262626] rounded-full'>
+                            <LockKeyhole  className='w-8 h-8 text-white' />
+                        </div>
+                        <div className='text-center text-[#262626]'>
+                            <h1 className='text-2xl'>Safe and Private</h1>
+                            <p>Enjoy your safety when connecting with customers.</p>
+                        </div>
+                    </div>
+                    
+                    <div></div>
+                    <div></div>
+                </div>
+                <div className=' flex flex-col gap-5  sm:w-[35%] sm:ml-8 text-[#262626]'>
+
+                    <h1 className='text-4xl lg:text-6xl font-semibold  '>
+                        Why <span className='text-[#250066]'>Talkify</span>
+                    </h1>
+                    <p className='text-sm sm:text-base'>Connect with your freinds   easily, quickly, and of course  have special features</p>
+                    <button className='flex w-fit  items-center gap-3 bg-[#250066] text-white rounded-md px-4 py-2 ease-in-out duration-500 hover:scale-105'>
+                        <span className='text-sm sm:text-base font-semibold '>Try it Now</span>
+                        <MoveRight className='h-5 w-5 ' />
+                    </button>
+                </div>
+
+
             </section>
 
         </main>
