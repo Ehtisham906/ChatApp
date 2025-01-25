c0695f5675a1d032338c45457782d6b2c2d54f05
Ehtisham Zahid <ehtishamzahid039@gmail.com>
2025-01-25 11:38:11 +0500

    images

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -1,6 +1,5 @@
 import { useState } from "react";
-import { useAuthStore } from "../store/useAuthStore";
-import AuthImagePattern from "../components/AuthImagePattern";
+import { useAuthStore } from "../store/useAuthStore"; 
 import HeroImage from "../assets/images/hero.png";
 import { Link } from "react-router-dom";
 import { Eye, EyeOff, Loader2, Lock, Mail, LucidePodcast } from "lucide-react";
