b43f29cd921e369429d30b644c8bea13eff94369
Ehtisham Zahid <ehtishamzahid039@gmail.com>
2025-01-26 12:34:02 +0500

    modified:   backend/src/index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -33,10 +33,10 @@ app.use("/api/auth", authRoutes)
 app.use("/api/messages", messageRoutes)
 
 if (process.env.NODE_ENV === "development") {
-    app.use(express.static(path.join(__dirname, "../../frontend/dist")));
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
     app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname, "../../frontend", "dist", "index.html"))
+        res.sendFile(path.resolve(__dirname, "../frontend", "dist", "index.html"))
     })
 }
 
